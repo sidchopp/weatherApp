f21f634e51c1c6ec8cfece8cce14ca1b3d5459bb
Sid <sidchopp@gmail.com>
2021-10-05 15:28:58 -0600

    spring and fall also included, colors based on images,images based on season

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,26 +1,44 @@
 import React from 'react';
 import { Card, Icon, Image, Header } from 'semantic-ui-react'
 
-import summerPic from './images/summer.jpg'
-import winterPic from './images/winter.jpg'
+import summerPic from './images/summer.jpg';
+import winterPic from './images/winter.jpg';
+import fallPic from './images/fall.jpg';
+import springPic from './images/spring.jpg';
 
 //CSS
 import './SeasonDisplay.css';
 
 const seasonConfig = {
   summer: {
-    text: "Let's hit the beach!!",
-    iconName: 'sun'
+    text: "Let's hit the beach & live in the sunshine.",
+    iconName: 'sun',
+    img: `${summerPic}`
   },
   winter: {
-    text: "Burrr.. it's cold!!",
-    iconName: 'snowflake'
-  }
+    text: "No winter lasts forever & no spring skips its turn.",
+    iconName: 'snowflake',
+    img: `${winterPic}`
+  },
+  fall: {
+    text: " Autumn leaves shower like gold, like rainbows, as the winds of change begin to blow.",
+    iconName: 'sun',
+    img: `${fallPic}`
+  },
+  spring: {
+    text: "A lovely reminder of how beautiful 'Change' can truly be.",
+    iconName: 'sun',
+    img: `${springPic}`
+  },
 };
 
 const getSeason = (lat, month) => {
-  if (month > 2 && month < 9) {
+  if (month >= 3 && month <= 5) {
+    return lat > 0 ? 'spring' : 'fall';
+  } else if (month >= 6 && month <= 8) {
     return lat > 0 ? 'summer' : 'winter';
+  } else if (month >= 9 && month <= 11) {
+    return lat > 0 ? 'fall' : 'spring';
   } else {
     return lat > 0 ? 'winter' : 'summer';
   }
@@ -28,8 +46,8 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = props => {
   const season = getSeason(props.lat, new Date().getMonth());
-  // console.log(season);
-  const { text, iconName } = seasonConfig[season];
+  console.log(season);
+  const { text, iconName, img } = seasonConfig[season];
 
   return (
     <div className={`season-display ${season}`}>
@@ -38,11 +56,12 @@ const SeasonDisplay = props => {
         {/* <i className={`icon-right large  ${iconName} icon`} /> */}
 
         {/* Conditional rendering of images acc to season */}
-        {season === 'winter'
+        {/* {season === 'winter'
           ? <Image src={winterPic} wrapped ui={false} />
-          : <Image src={summerPic} wrapped ui={false} />}
+          : <Image src={summerPic} wrapped ui={false} />} */}
         {/*  */}
 
+        <Image src={img} wrapped ui={false} />
         <Card.Content>
           <Card.Header>
             <span className='season-name'><Icon name={iconName} />{season} </span>
