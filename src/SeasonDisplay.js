bd8b736139e81601f798a3edc7a23b479e1a5469
Sid <sidchopp@gmail.com>
2021-10-05 12:09:44 -0600

    old App revived using React Semantic Ui

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Card, Icon, Image, Button } from 'semantic-ui-react'
 
 //CSS
 import './SeasonDisplay.css';
@@ -29,9 +30,33 @@ const SeasonDisplay = props => {
 
   return (
     <div className={`season-display ${season}`}>
-      <i className={`icon-left massive ${iconName} icon`} />
-      <h1>{text}</h1>
-      <i className={`icon-right massive ${iconName} icon`} />
+      <Card centered raised>
+        {/* <i className={`icon-left large  ${iconName} icon`} /> */}
+        <i className={`icon-right large  ${iconName} icon`} />
+
+        {/* Conditional rendering of images acc to season */}
+        {season === 'winter' ? <Image src='https://images.unsplash.com/photo-1529973625058-a665431328fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80' wrapped ui={false} /> : <Image src='https://images.unsplash.com/photo-1614717668397-5252f17ee5b5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80'
+          wrapped ui={false} />}
+        {/*  */}
+
+        <Card.Content>
+          <Card.Header>
+            <span className='season-name'>{season} </span>
+          </Card.Header>
+          <Card.Meta>
+            {text}
+          </Card.Meta>
+          <Card.Description>
+            <div>Latitude: {props.lat}</div>
+            <div>Longitude: {props.long}</div>
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <Button primary size='small' as='a' href='https://sid-projects.netlify.app/' target='_blank' rel="noreferrer" >
+            About Me
+          </Button>
+        </Card.Content>
+      </Card>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
 
+import 'semantic-ui-css/semantic.min.css'
+
 class App extends React.Component {
-  state = { lat: null, errorMessage: '' };
+  state = { lat: null, long: null, errorMessage: '' };
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => this.setState({ lat: position.coords.latitude }),
+      position => {
+        this.setState({ lat: position.coords.latitude, long: position.coords.longitude })
+        console.log(position.coords);
+      },
       err => this.setState({ errorMessage: err.message })
     );
   }
@@ -19,7 +24,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />;
+      return <SeasonDisplay lat={this.state.lat} long={this.state.long} />;
     }
 
     return <Spinner message="Please!! accept location request" />;
