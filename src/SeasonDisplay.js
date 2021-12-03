7a8f44c72e66a2144173960bbfba9fee8b10de8d
Sid <sidchopp@gmail.com>
2021-12-02 20:26:39 -0700

    Month error corrected

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -45,7 +45,7 @@ const getSeason = (lat, month) => {
 };
 
 const SeasonDisplay = props => {
-  const season = getSeason(props.lat, new Date().getMonth());
+  const season = getSeason(props.lat, new Date().getMonth() + 1);
   // console.log(season);
   const { text, iconName, img } = seasonConfig[season];
 
